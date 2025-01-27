a7845f0fce8b0dfa1a6fb7ad7226e2f1f36f0ffd
GabrielLima2803 <gabriellima2803@gmail.com>
2025-01-27 15:33:25 -0300

    Iniciando Projeto

---
+++ src/main/java/com/lima/GerenciamentoDePlanos/GerenciamentoDePlanosApplication.java
@@ -0,0 +1,13 @@
+package com.lima.GerenciamentoDePlanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDePlanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDePlanosApplication.class, args);
+	}
+
+}
