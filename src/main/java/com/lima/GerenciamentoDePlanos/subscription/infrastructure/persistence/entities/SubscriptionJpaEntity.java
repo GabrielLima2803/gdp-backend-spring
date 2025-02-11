d8a6053f7da17add5fdc9af2f19b9c6e0cace7c8
GabrielLima2803 <gabriellima2803@gmail.com>
2025-02-11 15:15:07 -0300

    feat(infra) - Adição da FK de payment em SubscriptionJpaEntity

--- src/main/java/com/lima/GerenciamentoDePlanos/subscription/infrastructure/persistence/entities/SubscriptionJpaEntity.java
+++ src/main/java/com/lima/GerenciamentoDePlanos/subscription/infrastructure/persistence/entities/SubscriptionJpaEntity.java
@@ -1,5 +1,6 @@
 package com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.entities;
 
+import com.lima.GerenciamentoDePlanos.payment.infrastructure.persistence.entities.PaymentJpaEntity;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -7,6 +8,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -25,6 +28,8 @@ public class SubscriptionJpaEntity {
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+    @OneToMany(mappedBy = "subscription", cascade = CascadeType.ALL)
+    private Set<PaymentJpaEntity> payments = new HashSet<>();
 
     public SubscriptionJpaEntity() {
     }
