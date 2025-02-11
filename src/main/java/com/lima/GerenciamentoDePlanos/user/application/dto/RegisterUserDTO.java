a070ee2ad1d817f479dd53cec95ce9e59a1759e8
GabrielLima2803 <gabriellima2803@gmail.com>
2025-02-11 15:13:32 -0300

    feat(app) - Criação de DTOS para User e Payment

---
+++ src/main/java/com/lima/GerenciamentoDePlanos/user/application/dto/RegisterUserDTO.java
@@ -0,0 +1,44 @@
+package com.lima.GerenciamentoDePlanos.user.application.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class RegisterUserDTO {
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public RegisterUserDTO() {
+    }
+    public RegisterUserDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
