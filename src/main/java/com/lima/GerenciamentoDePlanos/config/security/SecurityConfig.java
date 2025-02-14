a19ecb674b16d91ea03e22bd735e7ac882d20816
GabrielLima2803 <gabriellima2803@gmail.com>
2025-02-14 20:53:07 -0300

    fix() - permitAll rotas

--- src/main/java/com/lima/GerenciamentoDePlanos/config/security/SecurityConfig.java
+++ src/main/java/com/lima/GerenciamentoDePlanos/config/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/users/**").permitAll()
                         .requestMatchers("/api/payments/**").permitAll()
                         .requestMatchers("/api/notifications/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
