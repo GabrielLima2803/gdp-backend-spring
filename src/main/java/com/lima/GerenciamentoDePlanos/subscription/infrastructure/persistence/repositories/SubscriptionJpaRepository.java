73946efbca7e6535bb407beac9ab129dfbbdfc43
GabrielLima2803 <gabriellima2803@gmail.com>
2025-02-23 22:59:03 -0300

    feat: Criando novas buscas de Subscription

--- src/main/java/com/lima/GerenciamentoDePlanos/subscription/infrastructure/persistence/repositories/SubscriptionJpaRepository.java
+++ src/main/java/com/lima/GerenciamentoDePlanos/subscription/infrastructure/persistence/repositories/SubscriptionJpaRepository.java
@@ -2,6 +2,16 @@ package com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.r
 
 import com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.entities.SubscriptionJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface SubscriptionJpaRepository extends JpaRepository<SubscriptionJpaEntity, Long> {
+    List<SubscriptionJpaEntity> findByExpirationDateBeforeAndActiveTrue(LocalDateTime date);
+
+    @Query("SELECT s FROM SubscriptionJpaEntity s WHERE s.active = true AND s.expirationDate < :now")
+    List<SubscriptionJpaEntity> findExpiredSubscriptions(@Param("now") LocalDateTime now);
+
 }
--- src/main/java/com/lima/GerenciamentoDePlanos/subscription/infrastructure/persistence/repositories/impl/SubscriptionRepositoryImpl.java
+++ src/main/java/com/lima/GerenciamentoDePlanos/subscription/infrastructure/persistence/repositories/impl/SubscriptionRepositoryImpl.java
@@ -1,11 +1,13 @@
-package com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.repositories;
+package com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.repositories.impl;
 
 import com.lima.GerenciamentoDePlanos.subscription.domain.models.Subscription;
 import com.lima.GerenciamentoDePlanos.subscription.domain.repositories.SubscriptionRepository;
 import com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.entities.SubscriptionJpaEntity;
 import com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.mapper.SubscriptionPersistenceMapper;
+import com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.repositories.SubscriptionJpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -36,5 +38,21 @@ public class SubscriptionRepositoryImpl implements SubscriptionRepository {
                 .stream()
                 .map(mapper::toDomain)
                 .collect(Collectors.toList());
+    }
+    public List<Subscription> findByExpirationDateBeforeAndActiveTrue(LocalDateTime date) {
+        return jpaRepository.findByExpirationDateBeforeAndActiveTrue(date)
+                .stream()
+                .map(mapper::toDomain)
+                .toList();
+    }
+
+    public List<Subscription> findExpiredSubscriptions(LocalDateTime now) {
+        return jpaRepository.findExpiredSubscriptions(now)
+                .stream()
+                .map(mapper::toDomain)
+                .toList();
+    }
+    public void delete(Subscription subscription) {
+        jpaRepository.delete(mapper.toJpaEntity(subscription));
     }
 }
