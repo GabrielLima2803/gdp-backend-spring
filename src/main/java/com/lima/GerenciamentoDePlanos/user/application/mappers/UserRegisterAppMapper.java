275442e716d32c204be80d0b5193191de2b4d93d
GabrielLima2803 <gabriellima2803@gmail.com>
2025-02-23 23:01:29 -0300

    feat: Adicionando freeSubscription no Mapper

--- src/main/java/com/lima/GerenciamentoDePlanos/user/application/mappers/UserRegisterAppMapper.java
+++ src/main/java/com/lima/GerenciamentoDePlanos/user/application/mappers/UserRegisterAppMapper.java
@@ -1,14 +1,26 @@
 package com.lima.GerenciamentoDePlanos.user.application.mappers;
 
+import com.lima.GerenciamentoDePlanos.subscription.domain.models.Subscription;
 import com.lima.GerenciamentoDePlanos.user.application.dto.RegisterUserDTO;
 import com.lima.GerenciamentoDePlanos.user.domain.emuns.UserStatus;
 import com.lima.GerenciamentoDePlanos.user.domain.models.User;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Component
 public class UserRegisterAppMapper {
+
+    Subscription freeSubscription = new Subscription(
+            1L,
+            "Free",
+            new BigDecimal("0.00"),
+            "Plano Free",
+            0,
+            LocalDateTime.now(),
+            null
+    );
 
     public User toDomain(RegisterUserDTO dto) {
         return new User(
@@ -18,6 +30,7 @@ public class UserRegisterAppMapper {
                 dto.getPassword(),
                 LocalDateTime.now(),
                 LocalDateTime.now(),
+                freeSubscription,
                 UserStatus.ACTIVE,
                 null
         );
