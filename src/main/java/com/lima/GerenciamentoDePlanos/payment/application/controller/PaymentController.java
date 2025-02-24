6fe698f324da42766f342657ddef65088b57108d
GabrielLima2803 <gabriellima2803@gmail.com>
2025-02-23 22:54:09 -0300

    feat: Criando novas rotas de pagamento e historico

--- src/main/java/com/lima/GerenciamentoDePlanos/payment/application/controller/PaymentController.java
+++ src/main/java/com/lima/GerenciamentoDePlanos/payment/application/controller/PaymentController.java
@@ -1,15 +1,18 @@
 package com.lima.GerenciamentoDePlanos.payment.application.controller;
 
 import com.lima.GerenciamentoDePlanos.payment.application.dto.CreatePaymentDTO;
+import com.lima.GerenciamentoDePlanos.payment.application.dto.PaymentDTO;
 import com.lima.GerenciamentoDePlanos.payment.application.dto.PaymentResponseDTO;
 import com.lima.GerenciamentoDePlanos.payment.application.usecases.PaymentUseCase;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentController {
@@ -29,6 +32,18 @@ public class PaymentController {
     @GetMapping
     public ResponseEntity<List<PaymentResponseDTO>> getAllPayments() {
         List<PaymentResponseDTO> response = paymentUseCase.getAllPayments();
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/pay/{paymentId}")
+    public ResponseEntity<PaymentResponseDTO> payPayment(@PathVariable @Valid Long paymentId) {
+        PaymentResponseDTO response = paymentUseCase.payPayment(paymentId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<PaymentDTO>> getPaymentHistory() {
+        List<PaymentDTO> response = paymentUseCase.getPaymentHistory();
         return ResponseEntity.ok(response);
     }
 }
