e176f5d280b4f1a2a0ccc2302af66111b5774b72
GabrielLima2803 <gabriellima2803@gmail.com>
2025-02-23 23:00:25 -0300

    feat: Criando novas buscas de User

--- src/main/java/com/lima/GerenciamentoDePlanos/user/infrastructure/persistence/repositories/UserJpaRepository.java
+++ src/main/java/com/lima/GerenciamentoDePlanos/user/infrastructure/persistence/repositories/UserJpaRepository.java
@@ -1,12 +1,15 @@
 package com.lima.GerenciamentoDePlanos.user.infrastructure.persistence.repositories;
 
+import com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.entities.SubscriptionJpaEntity;
 import com.lima.GerenciamentoDePlanos.user.infrastructure.persistence.entities.UserJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID> {
     Optional<UserJpaEntity> findByUsername(String username);
     Optional<UserJpaEntity> findByEmail(String email);
+    List<UserJpaEntity> findBySubscription(SubscriptionJpaEntity subscription);
 }
--- src/main/java/com/lima/GerenciamentoDePlanos/user/infrastructure/persistence/repositories/impl/UserRepositoryImpl.java
+++ src/main/java/com/lima/GerenciamentoDePlanos/user/infrastructure/persistence/repositories/impl/UserRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.lima.GerenciamentoDePlanos.user.infrastructure.persistence.repositories.impl;
 
+import com.lima.GerenciamentoDePlanos.subscription.domain.models.Subscription;
+import com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.entities.SubscriptionJpaEntity;
+import com.lima.GerenciamentoDePlanos.subscription.infrastructure.persistence.mapper.SubscriptionPersistenceMapper;
 import com.lima.GerenciamentoDePlanos.user.domain.models.User;
 import com.lima.GerenciamentoDePlanos.user.domain.repositories.UserRepository;
 import com.lima.GerenciamentoDePlanos.user.infrastructure.persistence.entities.UserJpaEntity;
@@ -7,6 +10,7 @@ import com.lima.GerenciamentoDePlanos.user.infrastructure.persistence.mapper.Use
 import com.lima.GerenciamentoDePlanos.user.infrastructure.persistence.repositories.UserJpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,10 +18,12 @@ import java.util.UUID;
 public class UserRepositoryImpl implements UserRepository {
     private final UserJpaRepository userJpaRepository;
     private final UserPersistenceMapper mapper;
+    private final SubscriptionPersistenceMapper subscriptionPersistenceMapper;
 
-    public UserRepositoryImpl(UserJpaRepository userJpaRepository, UserPersistenceMapper mapper) {
+    public UserRepositoryImpl(UserJpaRepository userJpaRepository, UserPersistenceMapper mapper, SubscriptionPersistenceMapper subscriptionPersistenceMapper) {
         this.userJpaRepository = userJpaRepository;
         this.mapper = mapper;
+        this.subscriptionPersistenceMapper = subscriptionPersistenceMapper;
     }
 
     @Override
@@ -40,8 +46,26 @@ public class UserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    public List<User> findBySubscription(Subscription subscription) {
+        SubscriptionJpaEntity subscriptionJpaEntity = subscriptionPersistenceMapper.toJpaEntity(subscription);
+        return userJpaRepository.findBySubscription(subscriptionJpaEntity)
+                .stream()
+                .map(mapper::toDomain)
+                .toList();
+    }
+    @Override
     public Optional<User> findById(UUID id) {
         return userJpaRepository.findById(id)
                 .map(mapper::toDomain);
+    }
+    @Override
+    public List<User> saveAll(List<User> users) {
+        List<UserJpaEntity> entities = users.stream()
+                .map(mapper::toJpaEntity)
+                .toList();
+        List<UserJpaEntity> savedEntities = userJpaRepository.saveAll(entities);
+        return savedEntities.stream()
+                .map(mapper::toDomain)
+                .toList();
     }
 }
